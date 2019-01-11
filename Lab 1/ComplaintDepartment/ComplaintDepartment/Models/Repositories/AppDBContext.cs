8c72e6c9fb328b08f8f3cd470c2afc612dad150d
Robert Moore <robert.ben.moore@gmail.com>
2019-01-11 10:46:48 -0800

    Initial commit

---
+++ Lab 1/ComplaintDepartment/ComplaintDepartment/Models/Repositories/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace ComplaintDepartment.Models.Repositories
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options){ }
+        public DbSet<Complaint> Complaints { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Lab 1/ComplaintDepartment/ComplaintDepartment/Models/Repositories/IComplaintRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplaintDepartment.Models.Repositories
+{
+    public interface IComplaintRepository
+    {
+        List<Complaint> Complaints { get; }
+        bool AddComplaint(Complaint complaint);
+        bool MarkComplete(Complaint complaint);
+        List<Complaint> GetClosedComplaints();
+        List<Complaint> GetOpenComplaints();
+        bool ToggleComplete(int id);
+        List<Complaint> Search(string phrase);
+        Complaint Search(int id);
+        bool DeleteComplaintByID(int id);
+        
+    }
+}
