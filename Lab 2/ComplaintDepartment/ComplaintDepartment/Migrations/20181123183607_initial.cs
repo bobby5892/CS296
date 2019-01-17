cab649de115ec7dc71a781386ef9d7529b2dcd60
Robert Moore <robert.ben.moore@gmail.com>
2019-01-16 16:43:02 -0800

    Cleanup/Initial

---
+++ Lab 2/ComplaintDepartment/ComplaintDepartment/Migrations/20181123183607_initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ComplaintDepartment.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ComplaintID = table.Column<int>(nullable: false),
+                    Contents = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Complaints",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Create = table.Column<DateTime>(nullable: false),
+                    Contents = table.Column<string>(nullable: true),
+                    Completed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Complaints", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Complaints");
+        }
+    }
+}
---
+++ Lab 2/ComplaintDepartment/ComplaintDepartment/Models/Repositories/ICommentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplaintDepartment.Models.Repositories
+{
+    public interface ICommentRepository
+    {
+        List<Comment> Comments { get; }
+        List<Comment> GetCommentsByComplaint(int id);
+        bool AddComment(Comment comment);
+        bool DeleteCommentById(int id);
+        bool DeleteCommentsByComplaintID(int id);
+    }
+}
