94c6a8d3d8074b5c61d208a49e14c19f26b9e428
Robert Moore <robert.ben.moore@gmail.com>
2019-02-23 08:52:39 -0800

    Rest
    JS - Modified Fetch() to use appropriate REST HTTP Request_Type
    Controller - Added Edit Complaint Method
    Controller - Swapped HTTP Request Annotations to match appropriate REST operation

---
+++ Lab 7/ComplaintDepartment/ComplaintDepartment/Models/Repositories/IComplaintRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplaintDepartment.Models.Repositories
+{
+    public interface IComplaintRepository
+    {
+        List<Complaint> Complaints { get; }
+        bool AddComplaint(Complaint complaint);
+        bool MarkComplete(Complaint complaint);
+        List<Complaint> GetClosedComplaints();
+        List<Complaint> GetOpenComplaints();
+        bool ToggleComplete(int id);
+        List<Complaint> Search(string phrase);
+        Complaint Search(int id);
+        bool DeleteComplaintByID(int id);
+        bool EditComplaint(Complaint complaint);
+
+
+    }
+}
