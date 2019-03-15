352940d83ffb8fbb11f09c9b90e3dea7d6ac3a75
Robert Moore <robert.ben.moore@gmail.com>
2019-03-15 13:05:46 -0700

    docker
    docker

---
+++ Lab 9/ComplaintDepartment/ComplaintDepartment/Controllers/ComplaintController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ComplaintDepartment.Models;
+using ComplaintDepartment.Models.Repositories;
+using ComplaintDepartment.Models.View;
+namespace ComplaintDepartment.Controllers
+{
+    
+    public class ComplaintController : Controller
+    {
+        public ICommentRepository CommentRepo { get; set; }
+        public IComplaintRepository ComplaintRepo{ get; set; }
+        public ComplaintController(ICommentRepository commentRepository, IComplaintRepository complaintRepository)
+        {
+            this.CommentRepo = commentRepository;
+            this.ComplaintRepo = complaintRepository;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(string contents)
+        {
+            Complaint newComplaint = new Complaint()
+            {
+                Completed = false,
+                Contents = contents,
+                Create = DateTime.Now
+            };
+            this.ComplaintRepo.AddComplaint(newComplaint);
+            return View("ComplaintAdded",newComplaint);
+        }
+        public IActionResult List()
+        {
+            CommentsAndComplaints viewModel = new CommentsAndComplaints() { Comments = this.CommentRepo, Complaints = this.ComplaintRepo };
+            return View(viewModel);
+        }
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Search(string search)
+        {
+            if(search.Length == 0)
+            {
+                search = "42";
+            }
+            return View("SearchResults",this.ComplaintRepo.Search(search));
+        }
+        public IActionResult Comment()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetComplaint(int id)
+        {
+            /*ComplaintAndComments complaintAndComments = new ComplaintAndComments()
+            {
+                Complaint = this.ComplaintRepo.Search(id),
+                Comments = this.CommentRepo.GetCommentsByComplaint(id)
+            };
+            return View(complaintAndComments);*/
+            return Json(this.ComplaintRepo.Search(id));
+        }
+        [HttpPatch]
+        public JsonResult ToggleComplete(int id)
+        {
+            return Json(this.ComplaintRepo.ToggleComplete(id));
+        }
+        [HttpPost]
+        public JsonResult AddComment(int id, string content)
+        {
+            return Json(
+                this.CommentRepo.AddComment(
+                    new Comment() { ComplaintID = id, Contents = content }
+                    )
+                );
+        }
+        [HttpPut]
+        public JsonResult EditComplaint (int id, DateTime create, string contents, bool completed)
+        {
+            return Json(
+                this.ComplaintRepo.EditComplaint(new Complaint() { ID = id, Create = create, Contents = contents, Completed = completed })
+            );
+        }
+        [HttpDelete]
+        public JsonResult DeleteCommentById(int id)
+        {
+            return Json(
+               this.CommentRepo.DeleteCommentById(id)
+            );
+        }
+        [HttpDelete]
+        public JsonResult DeleteComplaintByID(int id)
+        {
+            return Json(
+                (this.CommentRepo.DeleteCommentsByComplaintID(id),
+                this.ComplaintRepo.DeleteComplaintByID(id))
+            );
+        }
+        [HttpPost]
+        public JsonResult AddComplaint(string contents)
+        {
+            Complaint newComplaint = new Complaint()
+            {
+                Completed = false,
+                Contents = contents,
+                Create = DateTime.Now
+            };
+            return Json(this.ComplaintRepo.AddComplaint(newComplaint));
+           
+        }
+        [HttpGet]
+        public JsonResult GetComplaints()
+        {
+            return Json(this.ComplaintRepo.Complaints.ToArray());
+        }
+        [HttpGet]
+        public JsonResult GetComments()
+        {
+            return Json(this.CommentRepo.Comments.ToArray());
+        }
+        [HttpPost]
+        public JsonResult GetCommentsByComplaint(int id)
+        {
+            var thing = this.CommentRepo.GetCommentsByComplaint(id).ToArray();
+            return Json(thing);
+        }
+    }
+}
